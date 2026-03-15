f4360a79fd7267cce8ca8731a3e05d6ed1c89fe1
reqini <reqini@gmail.com>
2026-03-15 12:08:12 -0300

    solucion images and videos 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/natyrodriguezok/", // <- importante para GitHub Pages
   root: path.resolve(__dirname, "client"),
-
-  // Esto es crucial para GitHub Pages
-  base: "/natyrodriguezok/",
-
-  server: {
-    host: "::",
-    port: 8080,
-    fs: { allow: [".."] },
-  },
-
-  build: {
-    outDir: path.resolve(__dirname, "dist/spa"),
-    emptyOutDir: true,
-  },
-
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client"),
       "@shared": path.resolve(__dirname, "shared"),
     },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist/spa"),
+    emptyOutDir: true,
   },
 });
